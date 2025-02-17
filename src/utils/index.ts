61a30d0f69432ce190f5a3d5b7dc7fd38d86573d
Alinafe Maliro <alinafemaliro@gmail.com>
2025-02-17 20:18:49 +0200

    add remove circular dependency util

---
+++ src/utils/index.ts
@@ -0,0 +1,24 @@
+export const removeCircularDependency =
+  <T extends object = object,>(obj: T, seen: Map<any, boolean> = new Map<any, boolean>()): Partial<T> => {
+  if (typeof obj !== 'object' || obj === null) {
+    return obj;
+  }
+
+  seen.set(obj, true);
+  const entries = Object.entries(obj);
+  entries.forEach(([key, value]): void => {
+    if (typeof value !== 'object') {
+      return;
+    }
+
+    if (seen.has(value)) {
+      delete obj[key as keyof T];
+      return;
+    }
+    
+    seen.set(value, true);
+    removeCircularDependency(value, seen)
+  })
+
+  return obj;
+}
---
+++ tests/unit/utils/index.test.ts
@@ -0,0 +1,31 @@
+import { removeCircularDependency } from 'src/utils'
+
+describe ('Utils', () => {
+  describe('#removeCircularDependency', () => {
+    it ('should remove circular dependency', () => {
+      const a = {
+        value1: 1,
+        a: null,
+        b: null,
+      };
+
+      // @ts-ignore
+      a.a = a;
+
+      const b = {
+        value2: 2,
+        b: null,
+        a: null,
+      }
+
+      // @ts-ignore
+      b['a'] = a;
+      // @ts-ignore
+      a['b'] = b;
+      // @ts-ignore
+      b['b'] = b;
+      const fixed = removeCircularDependency(a);
+      expect(fixed).toMatchObject({ value1: 1, b: { value2: 2 }})
+    })
+  })
+})
