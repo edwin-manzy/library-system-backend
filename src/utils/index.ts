4520dfcfce23edc4317d4a5e7d7f6a3167ebb168
Alinafe Maliro <alinafemaliro@gmail.com>
2025-02-17 20:30:42 +0200

    move buildSuccesResponse to json folder

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,24 +1,2 @@
-export const removeCircularDependency =
-  <T = object,>(obj: T, seen: Map<any, boolean> = new Map<any, boolean>()): Partial<T> => {
-  if (typeof obj !== 'object' || obj === null) {
-    return obj;
-  }
-
-  seen.set(obj, true);
-  const entries = Object.entries(obj);
-  entries.forEach(([key, value]): void => {
-    if (typeof value !== 'object') {
-      return;
-    }
-
-    if (seen.has(value)) {
-      delete obj[key as keyof T];
-      return;
-    }
-
-    seen.set(value, true);
-    removeCircularDependency(value, seen)
-  })
-
-  return obj;
-}
+export * from './json';
+export * from './express';
