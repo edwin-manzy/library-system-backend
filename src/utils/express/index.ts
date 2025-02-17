e957c7af4e6d76fec0034830462e7ba69edc17ec
Alinafe Maliro <alinafemaliro@gmail.com>
2025-02-17 20:29:21 +0200

    add buildSuccesresponse

--- src/utils/express/index.ts
+++ src/utils/express/index.ts
@@ -1,4 +1,4 @@
-import { removeCircularDependency } from ".."
+import { removeCircularDependency } from "src/utils/json"
 
 export const buildSuccessfulJsonResponse = <T=object,>(data: T): {data: Partial<T>} => {
   removeCircularDependency(data);
